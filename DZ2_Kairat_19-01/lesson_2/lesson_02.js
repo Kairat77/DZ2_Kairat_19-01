8236dc5ed2e954f56046c38bfc8da4f6045c1865
kairat <63288004+Kairat77@users.noreply.github.com>
2022-10-07 16:22:36 +0600

    Add files via upload

---
+++ DZ2_Kairat_19-01/lesson_2/lesson_02.js
@@ -0,0 +1,20 @@
+//Цикл 
+let i = 0;
+while (i < 51) {
+    alert(i);
+    console.log(i);
+    i++;
+}
+
+//Массив
+let arr = ["Привет", "Мир", "!"]
+alert(arr);
+console.log(arr);
+
+//Обьекты
+let user = {
+    name: "Kairat",
+    age: 21,
+    addres: "New Yourk",
+}
+console.log(user);
